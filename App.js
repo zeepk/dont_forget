091107c067738e3818eecdff90a9c59372893be1
unknown <mhughes.ipod@gmail.com>
2020-05-25 22:19:31 -0700

    init

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return <Text>Nice</Text>;
+};
+
+export default App;
